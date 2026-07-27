4561194e451f9dee31134cf02bfe1652d20aff40
Josue Jimenez <jjimeneza.act@gmail.com>
2026-07-26 18:48:31 -0600

    Reconfiguracion de CORS

--- scentia-backend/app/main.py
+++ scentia-backend/app/main.py
@@ -23,11 +23,15 @@ app = FastAPI(
     #allow_headers=["*"],
 #)
 
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://scentia-prod.vercel.app",  # Tu URL de producción en Vercel
+]
 
-# Configuración de CORS con RegEx para admitir Vercel (Producción y Previews) y Localhost
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"https://.*\.vercel\.app|http://(localhost|127\.0\.0\.1)(:\d+)?",
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
